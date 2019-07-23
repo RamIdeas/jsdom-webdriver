cfae9ff15b292ddbb74e48de30985c859dcedc6c
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2019-07-23 11:43:45 +0100

    adapt webdriver for jsdom

--- webdriver.js
+++ webdriver.js
@@ -9,17 +9,23 @@ const webdriver = require('selenium-webdriver/lib/webdriver');
 const { Browser, Capabilities } = require('selenium-webdriver/lib/capabilities');
 
 /**
- * _Synchronously_ attempts to locate the edge driver executable on the current
+ * _Synchronously_ attempts to locate the jsdom driver executable on the current
  * system.
  *
  * @return {?string} the located executable, or `null`.
  */
 function locateSynchronously() {
-    return process.platform === 'win32' ? io.findInPath(EDGEDRIVER_EXE, true) : null;
+    const filename = {
+        win32: 'jsdom-webdriver-win.exe',
+        darwin: 'jsdom-webdriver-macos',
+        linux: 'jsdom-webdriver-linux',
+    }[process.platform];
+
+    return path.resolve(__dirname, './bin/' + filename);
 }
 
 /**
- * Class for managing MicrosoftEdgeDriver specific options.
+ * Class for managing JSDOMDriver specific options.
  */
 class Options extends Capabilities {
     /**
@@ -28,34 +34,21 @@ class Options extends Capabilities {
      */
     constructor(other = undefined) {
         super(other);
-        this.setBrowserName(Browser.EDGE);
+        this.setBrowserName('JSDOM');
     }
 }
 
 /**
  * Creates {@link remote.DriverService} instances that manage a
- * MicrosoftEdgeDriver server in a child process.
+ * JSDOMDriver server in a child process.
  */
 class ServiceBuilder extends remote.DriverService.Builder {
     /**
      * @param {string=} opt_exe Path to the server executable to use. If omitted,
-     *   the builder will attempt to locate the MicrosoftEdgeDriver on the current
-     *   PATH.
-     * @throws {Error} If provided executable does not exist, or the
-     *   MicrosoftEdgeDriver cannot be found on the PATH.
+     *   the builder will use the locally installed binary.
      */
     constructor(opt_exe) {
         let exe = opt_exe || locateSynchronously();
-        if (!exe) {
-            throw Error(
-                'The ' +
-                    EDGEDRIVER_EXE +
-                    ' could not be found on the current PATH. ' +
-                    'Please download the latest version of the MicrosoftEdgeDriver from ' +
-                    'https://www.microsoft.com/en-us/download/details.aspx?id=48212 and ' +
-                    'ensure it can be found on your PATH.',
-            );
-        }
 
         super(exe);
 
@@ -78,14 +71,14 @@ class ServiceBuilder extends remote.DriverService.Builder {
 var defaultService = null;
 
 /**
- * Sets the default service to use for new MicrosoftEdgeDriver instances.
+ * Sets the default service to use for new JSDOMDriver instances.
  * @param {!remote.DriverService} service The service to use.
  * @throws {Error} If the default service is currently running.
  */
 function setDefaultService(service) {
     if (defaultService && defaultService.isRunning()) {
         throw Error(
-            'The previously configured EdgeDriver service is still running. ' +
+            'The previously configured JSDOMDriver service is still running. ' +
                 'You must shut it down before you may adjust its configuration.',
         );
     }
@@ -93,10 +86,10 @@ function setDefaultService(service) {
 }
 
 /**
- * Returns the default MicrosoftEdgeDriver service. If such a service has
+ * Returns the default JSDOMDriver service. If such a service has
  * not been configured, one will be constructed using the default configuration
- * for an MicrosoftEdgeDriver executable found on the system PATH.
- * @return {!remote.DriverService} The default MicrosoftEdgeDriver service.
+ * for a JSDOMDriver executable found on the system PATH.
+ * @return {!remote.DriverService} The default JSDOMDriver service.
  */
 function getDefaultService() {
     if (!defaultService) {
@@ -106,16 +99,16 @@ function getDefaultService() {
 }
 
 /**
- * Creates a new WebDriver client for Microsoft's Edge.
+ * Creates a new WebDriver client for JSDOM.
  */
-class Driver extends webdriver.WebDriver {
+class JSDOMDriver extends webdriver.WebDriver {
     /**
-     * Creates a new browser session for Microsoft's Edge browser.
+     * Creates a new browser session in JSDOM and starts the service, if not already started.
      *
      * @param {(Capabilities|Options)=} options The configuration options.
      * @param {remote.DriverService=} service The session to use; will use
      *     the {@linkplain #getDefaultService default service} by default.
-     * @return {!Driver} A new driver instance.
+     * @return {!JSDOMDriver} A new driver instance.
      */
     static createSession(options, opt_service) {
         let service = opt_service || getDefaultService();
@@ -123,7 +116,22 @@ class Driver extends webdriver.WebDriver {
         let executor = new http.Executor(client);
 
         options = options || new Options();
-        return /** @type {!Driver} */ (super.createSession(executor, options, () => service.kill()));
+        return /** @type {!JSDOMDriver} */ (super.createSession(executor, options, () => service.kill()));
+    }
+
+    /**
+     * Creates a new browser session in JSDOM.
+     *
+     * @param {(Capabilities|Options)=} options The configuration options.
+     * @param {string} url the address of an existing JSDOM server instance e.g. http://localhost:3000
+     * @return {!JSDOMDriver} A new driver instance.
+     */
+    static createSessionWithExistingService(options, url) {
+        let client = new http.HttpClient(url);
+        let executor = new http.Executor(client);
+
+        options = options || new Options();
+        return /** @type {!JSDOMDriver} */ (super.createSession(executor, options));
     }
 
     /**
@@ -136,7 +144,7 @@ class Driver extends webdriver.WebDriver {
 
 // PUBLIC API
 
-exports.Driver = Driver;
+exports.Driver = JSDOMDriver;
 exports.Options = Options;
 exports.ServiceBuilder = ServiceBuilder;
 exports.getDefaultService = getDefaultService;
